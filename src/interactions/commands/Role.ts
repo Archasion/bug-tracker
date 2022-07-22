991fdf4939a003d0067d5fb36b44f7ef8af7d14b
Nick <james.nswork@gmail.com>
2022-07-23 00:54:47 +0100

    Implemented an `info` subcommand

--- src/interactions/commands/Role.ts
+++ src/interactions/commands/Role.ts
@@ -2,7 +2,7 @@ import Command from "../../modules/interactions/commands/Command";
 import Guilds from "../../db/models/Guilds";
 import Bot from "../../Bot";
 
-import { ApplicationCommandChoicesData, ApplicationCommandOptionType, ChatInputCommandInteraction } from "discord.js";
+import { ApplicationCommandChoicesData, ApplicationCommandOptionType, ApplicationCommandType, ChatInputCommandInteraction, EmbedBuilder, Role } from "discord.js";
 import { RestrictionLevel } from "../../utils/RestrictionUtils";
 
 const roleType: ApplicationCommandChoicesData = {
@@ -32,6 +32,7 @@ export default class RoleCommand extends Command {
 			name: "role",
 			description: "Manage the roles configured for the bot.",
 			restriction: RestrictionLevel.Administrator,
+                  type: ApplicationCommandType.ChatInput,
 			options: [
                         {
                               name: "set",
@@ -58,6 +59,19 @@ export default class RoleCommand extends Command {
 					description: "Remove a role configuration.",
                               type: ApplicationCommandOptionType.Subcommand,
                               options: [roleType]
+				},
+                        {
+                              name: "info",
+					description: "Get information on the chosen role.",
+                              type: ApplicationCommandOptionType.Subcommand,
+                              options: [
+                                    {
+                                          name: "role",
+                                          description: "The role to view information on.",
+                                          type: ApplicationCommandOptionType.Role,
+                                          required: true
+                                    }
+                              ]
 				}
 			]
 		});
@@ -70,11 +84,10 @@ export default class RoleCommand extends Command {
       async execute(interaction: ChatInputCommandInteraction): Promise<void> {    
             const action = interaction.options.getSubcommand();
 		const type = interaction.options.getString("type") as string;
+            const role = interaction.options.getRole("role") as Role;
 
             switch (action) {
                   case "set":
-                        const role = interaction.options.getRole("role");
-
                         await Guilds.updateOne({ id: interaction.guildId }, { $set: { [`roles.${type}`]: role!.id } });
                         interaction.editReply(`The **${type}** role has been set to ${role}.`);
                         break;
@@ -94,6 +107,53 @@ export default class RoleCommand extends Command {
                         }
 
                         interaction.editReply(`The **${type.replace(/_/g, " ")}** role is set to <@&${roleId}>.`);
+                        break;
+
+                  case "info":
+                        const embed = new EmbedBuilder()
+                              .setColor(role!.color)
+                              .setTitle(role!.name)
+                              .setThumbnail((role as Role).iconURL())
+                              .setFooter({ text: `ID: ${role!.id}` })
+                              .setFields([
+                                    {
+                                          name: "Created",
+                                          value: `<t:${Math.round(role.createdTimestamp as number / 1000)}:R>`,
+                                          inline: true
+                                    },
+                                    {
+                                          name: "Color",
+                                          value: role.hexColor,
+                                          inline: true
+                                    },
+                                    {
+                                          name: "Members",
+                                          value: role.members.size.toString(),
+                                          inline: true
+                                    },
+                                    {
+                                          name: "Position",
+                                          value: role.position.toString(),
+                                          inline: true
+                                    },
+                                    {
+                                          name: "Mentionable",
+                                          value: role.mentionable.toString(),
+                                          inline: true
+                                    },
+                                    {
+                                          name: "Hoisted",
+                                          value: role.hoist.toString(),
+                                          inline: true
+                                    },
+                                    {
+                                          name: `Permissions (${role.permissions.toArray().length})`,
+                                          value: `\`${role.permissions.toArray().join("` `") || "None"}\``,
+                                          inline: false
+                                    }
+                              ]);
+
+                              interaction.editReply({ embeds: [embed] });
                         break;
             }
 
