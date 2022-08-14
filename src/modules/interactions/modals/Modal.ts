938d9988078b0677792e40a78aca3c4a663db55a
Nick <james.nswork@gmail.com>
2022-08-14 12:20:45 +0100

    Removed the `modalResponse?` option

--- src/modules/interactions/modals/Modal.ts
+++ src/modules/interactions/modals/Modal.ts
@@ -7,14 +7,12 @@ export default class Modal {
       client: Bot;
       manager: ModalHandler;
       restriction: RestrictionLevel;
-      modalResponse?: boolean;
       name: string | { startsWith: string } | { endsWith: string } | { includes: string };
       
-      constructor(client: Bot, data: { restriction: RestrictionLevel; modalResponse: boolean; name: string | { startsWith: string; } | { endsWith: string; } | { includes: string; } }) {
+      constructor(client: Bot, data: { restriction: RestrictionLevel; name: string | { startsWith: string; } | { endsWith: string; } | { includes: string; } }) {
             this.client = client;
             this.manager = client.modals;
             this.restriction = data.restriction;
-            this.modalResponse = data.modalResponse ?? false;
             this.name = data.name;
 
             try {
