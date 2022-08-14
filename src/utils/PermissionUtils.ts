460970da0b5e1e63232a46c31328eeb5e32796ed
Nick <james.nswork@gmail.com>
2022-08-14 22:06:21 +0100

    Changed the interaction type

--- src/utils/PermissionUtils.ts
+++ src/utils/PermissionUtils.ts
@@ -1,12 +1,19 @@
-import { CommandInteraction, GuildMember, MessageComponentInteraction, NewsChannel, PermissionResolvable, TextChannel } from "discord.js";
+import { AutocompleteInteraction, GuildMember, Interaction, NewsChannel, PermissionResolvable, TextChannel } from "discord.js";
  
 export default class PermissionUtils {
-      public static async botHasPermissions(interaction: CommandInteraction | MessageComponentInteraction, permissions: PermissionResolvable[], channel: TextChannel | NewsChannel = interaction.channel as TextChannel | NewsChannel): Promise<boolean> {
+      public static async botHasPermissions(interaction: Exclude<Interaction, AutocompleteInteraction>, permissions: PermissionResolvable[], channel: TextChannel | NewsChannel = interaction.channel as TextChannel | NewsChannel): Promise<boolean> {
             const client = interaction.guild?.members.me as GuildMember;
             const missingPermissions = permissions.filter(permission => !client.permissionsIn(channel).has(permission));
 
             if (missingPermissions.length > 0) {
-                  interaction.editReply(`I need the following permissions in ${channel} (\`${channel.id}\`):\n\`${missingPermissions.join("` `")}\``);
+                  try {
+                        interaction.editReply(`I need the following permissions in ${channel} (\`${channel.id}\`):\n\`${missingPermissions.join("` `")}\``);
+                  } catch {
+                        interaction.reply({
+                              content: `I need the following permissions in ${channel} (\`${channel.id}\`):\n\`${missingPermissions.join("` `")}\``,
+                              ephemeral: true
+                        });
+                  }
                   return false;
             }
 
