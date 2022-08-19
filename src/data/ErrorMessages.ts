f331abc3dc538062320f95fa4f96c5531664d2c1
Nick <james.nswork@gmail.com>
2022-08-19 23:08:56 +0100

    Added `SubmissionNotFound`

--- src/data/ErrorMessages.ts
+++ src/data/ErrorMessages.ts
@@ -1,4 +1,5 @@
 export default class ErrorMessages {
       public static readonly ChannelNotConfigured = "There is no channel configured for this action. An administrator is able to configure one using `/channel set`";
       public static readonly ChannelNotFound = "The configured channel for this action no longer exists. An administrator is able to configure one using `/channel set`";
+      public static readonly SubmissionNotFound = "Couldn't find the submission, it may have been deleted.";
 }
