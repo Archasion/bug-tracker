2e2f3dc74649c77b5d9f158b82f0ba05f2bec239
Nick <james.nswork@gmail.com>
2022-08-20 12:55:09 +0100

    Made `once` optional

--- src/modules/listeners/Listener.ts
+++ src/modules/listeners/Listener.ts
@@ -3,11 +3,11 @@ import Bot from "../../Bot";
 export default class EventListener {
       client: Bot;
       name: string;
-      once: boolean;
+      once?: boolean;
 
-      constructor(client: Bot, data: { name: string; once: boolean; }) {
+      constructor(client: Bot, data: { name: string; once?: boolean; }) {
             this.client = client;
             this.name = data.name;
-            this.once = data.once;
+            this.once = data.once ?? false;
       }
 }
