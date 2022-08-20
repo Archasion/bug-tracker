e44f5ed84be83a027572fb8482702df1b4c8b31e
Nick <james.nswork@gmail.com>
2022-08-20 12:54:43 +0100

    Implemented listener: `guildDelete`

---
+++ src/listeners/GuildDelete.ts
@@ -0,0 +1,23 @@
+import EventListener from "../modules/listeners/Listener";
+import Guild from "../db/models/Guild.model";
+import Properties from "../data/Properties";
+import clc from "cli-color";
+import Bot from "../Bot";
+
+import { Guild as DiscordGuild } from "discord.js";
+
+module.exports = class GuildDeleteEventListener extends EventListener {
+      constructor(client: Bot) {
+            super(client, { name: "guildDelete" });
+      }
+
+      public async execute(guild: DiscordGuild) {
+            try {
+                  await Guild.deleteOne({ id: guild.id })
+                        .then(() => console.log("%s Removed guild configuration %s", Properties.cli.listeners.guildDelete, clc.blackBright(`("${guild.name}" • ${guild.id})`)));
+            } catch (err) {
+                  console.log("%s Failed to remove guild configuration %s", Properties.cli.listeners.guildDelete, clc.blackBright(`("${guild.name}" • ${guild.id})`));
+                  console.error(err);
+            }
+      }
+};
