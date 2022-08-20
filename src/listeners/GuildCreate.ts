27a83b7d9cb9bdf6aeb3b0264e1caae48424869b
Nick <james.nswork@gmail.com>
2022-08-20 12:54:34 +0100

    Implemented listener: `guildCreate`

---
+++ src/listeners/GuildCreate.ts
@@ -0,0 +1,28 @@
+import EventListener from "../modules/listeners/Listener";
+import Guild from "../db/models/Guild.model";
+import Properties from "../data/Properties";
+import clc from "cli-color";
+import Bot from "../Bot";
+
+import { Guild as DiscordGuild } from "discord.js";
+
+module.exports = class GuildCreateEventListener extends EventListener {
+      constructor(client: Bot) {
+            super(client, { name: "guildCreate" });
+      }
+
+      public async execute(guild: DiscordGuild) {
+            const guildConfig = await Guild.findOne({ id: guild.id });
+
+            if (!guildConfig) {
+                  const newGuildConfig = new Guild({ id: guild.id });
+                  await newGuildConfig
+                        .save()
+                        .then(() => console.log("%s Created a new guild configuration %s", Properties.cli.listeners.guildCreate, clc.blackBright(`("${guild.name}" • ${guild.id})`)));
+                  
+                  return;
+            }
+
+            console.log("%s Joined guild %s", Properties.cli.listeners.guildCreate, clc.blackBright(`("${guild.name}" • ${guild.id})`));
+      }
+};
