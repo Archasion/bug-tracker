2e8066b4b81afdb34fe6e8aee557ce59629a0f5e
Nick <james.nswork@gmail.com>
2022-08-21 21:36:19 +0100

    Corrected length

--- src/utils/StringUtils.ts
+++ src/utils/StringUtils.ts
@@ -1,6 +1,6 @@
 export default class StringUtils {
       public static elipsify(str: string, maxLength: number): string {
-            if (str.length > maxLength) return str.substring(0, maxLength) + "...";
+            if (str.length > maxLength) return str.substring(0, maxLength - 3) + "...";
             return str;
       }
 }
