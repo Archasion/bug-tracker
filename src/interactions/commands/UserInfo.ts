00cd44af9c0e6e2d8be5435a3e713501e2a1f5d2
Nick <james.nswork@gmail.com>
2022-09-04 11:16:49 +0100

    Made the `member` argument required and fixed formatting

--- src/interactions/commands/UserInfo.ts
+++ src/interactions/commands/UserInfo.ts
@@ -3,9 +3,9 @@ import Properties from "../../data/Properties";
 import Guild from "../../db/models/Guild.model";
 import Bot from "../../Bot";
 
-import { 
+import {
       ApplicationCommandOptionType,
-      ChatInputCommandInteraction, 
+      ChatInputCommandInteraction,
       ApplicationCommandType,
       ActionRowBuilder,
       ButtonBuilder,
@@ -14,7 +14,7 @@ import {
       ButtonStyle
 } from "discord.js";
 
-import { RestrictionLevel } from "../../utils/RestrictionUtils";
+import {RestrictionLevel} from "../../utils/RestrictionUtils";
 
 export default class UserInfoCommand extends Command {
       constructor(client: Bot) {
@@ -27,106 +27,107 @@ export default class UserInfoCommand extends Command {
                         {
                               name: "member",
                               description: "The member to view information about.",
-                              type: ApplicationCommandOptionType.User
+                              type: ApplicationCommandOptionType.User,
+                              required: true
                         }
                   ]
             });
       }
 
-	/**
-	 * @param {ChatInputCommandInteraction} interaction
-	 * @returns {Promise<void>}
-	 */
+      /**
+       * @param {ChatInputCommandInteraction} interaction
+       * @returns {Promise<void>}
+       */
       async execute(interaction: ChatInputCommandInteraction): Promise<void> {
             const guildConfig = await Guild.findOne(
-                  { id: interaction.guildId }, 
-                  { 
-                        bugs: 1, 
-                        reports: 1, 
-                        suggestions: 1, 
-                        _id: 0 
+                  {id: interaction.guildId},
+                  {
+                        bugs: 1,
+                        reports: 1,
+                        suggestions: 1,
+                        _id: 0
                   }
             );
 
-		const member = interaction.options.getMember("member") as GuildMember;
-		await member.user.fetch(true);
+            const member = interaction.options.getMember("member") as GuildMember;
+            await member.user.fetch(true);
 
             // eslint-disable-next-line @typescript-eslint/ban-ts-comment
             // @ts-ignore
-            let { bugs, reports, suggestions } = guildConfig;
+            let {bugs, reports, suggestions} = guildConfig;
 
-		suggestions = suggestions.filter((suggestion: { author: string; }) => suggestion.author === member.id);
-		reports = reports.filter((report: { author: string; }) => report.author === member.id);
-		bugs = bugs.filter((bug: { author: string; }) => bug.author === member.id);
+            suggestions = suggestions.filter((suggestion: { author: string; }) => suggestion.author === member.id);
+            reports = reports.filter((report: { author: string; }) => report.author === member.id);
+            bugs = bugs.filter((bug: { author: string; }) => bug.author === member.id);
 
-		const permissions = member.permissions.toArray().join("` `") || "None";
-		const roles = member.roles.cache.map(role => role).join(" ") || "None";
+            const permissions = member.permissions.toArray().join("` `") || "None";
+            const roles = member.roles.cache.map(role => role).join(" ") || "None";
 
-		const info = new EmbedBuilder()
-			.setColor(Properties.colors.default)
-			.setTitle(member.displayName)
-			.setThumbnail(member.displayAvatarURL())
-			.setImage(member.user.bannerURL() as string | null)
-			.setFooter({ text: `ID: ${member.id}` })
-			.setAuthor({
-				name: member.user.tag,
-				iconURL: member.user.displayAvatarURL()
-			})
-			.setFields([
-				{
-					name: "Created",
-					value: `<t:${Math.round(member.user.createdTimestamp as number / 1000)}:R>`,
-					inline: true
-				},
-				{
-					name: "Joined",
-					value: `<t:${Math.round(member.joinedTimestamp as number / 1000)}:R>`,
-					inline: true
-				},
-				{
-					name: "All Reports",
-					value: (bugs.length + reports.length + suggestions.length).toString(),
-					inline: true
-				},
-				{
-					name: "Bug Reports",
-					value: bugs.length.toString(),
-					inline: true
-				},
-				{
-					name: "Player Reports",
-					value: reports.length.toString(),
-					inline: true
-				},
-				{
-					name: "Suggestions",
-					value: suggestions.length.toString(),
-					inline: true
-				},
-				{
-					name: `Roles (${member.roles.cache.size})`,
-					value: roles,
-					inline: false
-				},
-				{
-					name: `Permissions (${member.permissions.toArray().length})`,
-					value: `\`${permissions}\``,
-					inline: false
-				}
-			]);
+            const info = new EmbedBuilder()
+                  .setColor(Properties.colors.default)
+                  .setTitle(member.displayName)
+                  .setThumbnail(member.displayAvatarURL())
+                  .setImage(member.user.bannerURL() as string | null)
+                  .setFooter({text: `ID: ${member.id}`})
+                  .setAuthor({
+                        name: member.user.tag,
+                        iconURL: member.user.displayAvatarURL()
+                  })
+                  .setFields([
+                        {
+                              name: "Created",
+                              value: `<t:${Math.round(member.user.createdTimestamp as number / 1000)}:R>`,
+                              inline: true
+                        },
+                        {
+                              name: "Joined",
+                              value: `<t:${Math.round(member.joinedTimestamp as number / 1000)}:R>`,
+                              inline: true
+                        },
+                        {
+                              name: "All Reports",
+                              value: (bugs.length + reports.length + suggestions.length).toString(),
+                              inline: true
+                        },
+                        {
+                              name: "Bug Reports",
+                              value: bugs.length.toString(),
+                              inline: true
+                        },
+                        {
+                              name: "Player Reports",
+                              value: reports.length.toString(),
+                              inline: true
+                        },
+                        {
+                              name: "Suggestions",
+                              value: suggestions.length.toString(),
+                              inline: true
+                        },
+                        {
+                              name: `Roles (${member.roles.cache.size})`,
+                              value: roles,
+                              inline: false
+                        },
+                        {
+                              name: `Permissions (${member.permissions.toArray().length})`,
+                              value: `\`${permissions}\``,
+                              inline: false
+                        }
+                  ]);
 
-		const actionRow: ActionRowBuilder<ButtonBuilder> = new ActionRowBuilder().addComponents(
+            const actionRow: ActionRowBuilder<ButtonBuilder> = new ActionRowBuilder().addComponents(
                   new ButtonBuilder({})
-			.setLabel("Avatar")
-			.setStyle(ButtonStyle.Link)
-			.setURL(member.displayAvatarURL())
+                        .setLabel("Avatar")
+                        .setStyle(ButtonStyle.Link)
+                        .setURL(member.displayAvatarURL())
             ) as ActionRowBuilder<ButtonBuilder>;
 
-		await interaction.editReply({
-			content: `${member}`,
-			embeds: [info],
-			components: [actionRow]
-		});
+            await interaction.editReply({
+                  content: `${member}`,
+                  embeds: [info],
+                  components: [actionRow]
+            });
 
             return;
       }
