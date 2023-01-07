317ada6544613a0b5967090c4f834cc35832383c
Nick <james.nswork@gmail.com>
2023-01-07 20:21:03 +0000

    `Properties` -> `ClientProperties` + new location

--- src/data/Properties.ts
+++ src/data/Properties.ts
@@ -1,62 +1,7 @@
-import {PermissionFlagsBits, PermissionResolvable} from "discord.js";
+import {PermissionFlagsBits} from "discord.js";
 import {readFileSync} from "node:fs";
+import {ClientProperties} from "./Types";
 import {parse} from "yaml";
-
-type Properties = {
-    invitePermissions: PermissionResolvable
-    colors: {
-        default: 0x008EFF
-        success: 0x84F584
-        error: 0x202020
-
-        priority: {
-            High: 0xe86864
-            Medium: 0xe89d3a
-            Low: 0x71d17f
-            None: 0x4a4a4a
-        }
-
-        status: {
-            Approved: 0x437B4B
-            Rejected: 0xC74945
-            Known: 0xC74945
-            NAB: 0xC74945
-            Implemented: 0x5d97c9
-            Considered: 0xf2e65e
-            Fixed: 0x855dc9
-            None: 0x2F3136
-        }
-    }
-
-    users: {
-        developers: string[]
-    }
-
-    channels: {
-        contact: {
-            support: string
-            suggestions: string
-            bugs: string
-            feedback: string
-            other: string
-        }
-    }
-    emojis: {
-        thumbsUp: string
-        thumbsDown: string
-        approve: string
-        reject: string
-        known: string
-        nab: string
-        implemented: string
-        considered: string
-        fixed: string
-        none: string
-        edit: string
-        inbox: string
-        thread: string
-    }
-}
 
 const invitePermissions = [
     PermissionFlagsBits.SendMessagesInThreads,
@@ -72,7 +17,7 @@ const invitePermissions = [
     PermissionFlagsBits.EmbedLinks
 ].reduce((a, b) => a + b);
 
-const properties = parse(readFileSync("Properties.yaml", "utf8")) as Properties;
+const properties = parse(readFileSync("Properties.yaml", "utf8")) as ClientProperties;
 properties.invitePermissions = invitePermissions;
 
 export default properties;
--- src/data/Types.ts
+++ src/data/Types.ts
@@ -1,11 +1,71 @@
+import {ColorResolvable, PermissionResolvable} from "discord.js";
+
 type SubmissionStatus = "Approved" | "Rejected" | "Fixed" | "Considered" | "Implemented" | "None" | "Known" | "NAB";
-type SuggestionStatus = "Approved" | "Rejected" | "Considered" | "Implemented" | "None";
-type ContactEnquiry = "support" | "bugs" | "suggestions" | "feedback" | "other";
-type PlayerReportStatus = "Approved" | "Rejected" | "Considered" | "None" | "Known";
 type BugStatus = "Approved" | "Rejected" | "Fixed" | "Considered" | "None" | "Known" | "NAB";
-type Guide = "bug_reports" | "player_reports" | "suggestions";
+type SuggestionStatus = "Approved" | "Rejected" | "Considered" | "Implemented" | "None";
+type PlayerReportStatus = "Approved" | "Rejected" | "Considered" | "None" | "Known";
+
 type SubmissionType = "bugReports" | "playerReports" | "suggestions";
 type BugPriority = "None" | "Low" | "Medium" | "High";
+
+type ContactEnquiry = "support" | "bugs" | "suggestions" | "feedback" | "other";
+type Guide = "bug_reports" | "player_reports" | "suggestions";
+
+type ClientProperties = {
+    invitePermissions: PermissionResolvable
+    colors: {
+        default: ColorResolvable
+        success: ColorResolvable
+        error: ColorResolvable
+
+        priority: {
+            High: ColorResolvable
+            Medium: ColorResolvable
+            Low: ColorResolvable
+            None: ColorResolvable
+        }
+
+        status: {
+            Approved: ColorResolvable
+            Rejected: ColorResolvable
+            Known: ColorResolvable
+            NAB: ColorResolvable
+            Implemented: ColorResolvable
+            Considered: ColorResolvable
+            Fixed: ColorResolvable
+            None: ColorResolvable
+        }
+    }
+
+    users: {
+        developers: string[]
+    }
+
+    channels: {
+        contact: {
+            support: string
+            suggestions: string
+            bugs: string
+            feedback: string
+            other: string
+        }
+    }
+    emojis: {
+        thumbsUp: string
+        thumbsDown: string
+        approve: string
+        reject: string
+        known: string
+        nab: string
+        implemented: string
+        considered: string
+        fixed: string
+        none: string
+        edit: string
+        inbox: string
+        thread: string
+    }
+}
 
 export {
     PlayerReportStatus,
@@ -14,6 +74,7 @@ export {
     SubmissionType,
     ContactEnquiry,
     BugPriority,
+    ClientProperties,
     BugStatus,
     Guide
 };
